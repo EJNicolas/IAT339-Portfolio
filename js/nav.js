748c4e3781a5d4f1345645814d38e262d843dd83
EJ Nicolas <ejnicolas001@gmail.com>
2021-04-08 00:24:22 -0700

    Added nav animation

--- js/nav.js
+++ js/nav.js
@@ -7,11 +7,33 @@ var normalLogo = document.querySelector("#logo");
 var nav = document.querySelector("#side-nav");
 
 brightLogo.addEventListener("click", function(){
+  nav.classList.add("animate-nav-forward");
+  nav.classList.remove("animate-nav-back");
   nav.classList.remove("hidden");
   brightLogo.classList.add("hidden");
 });
 
+brightLogo.addEventListener("keypress", function(e){
+  if(e.key == 'Enter'){
+    nav.classList.add("animate-nav-forward");
+    nav.classList.remove("animate-nav-back");
+    nav.classList.remove("hidden");
+    brightLogo.classList.add("hidden");
+  }
+});
+
 normalLogo.addEventListener("click", function(){
-  nav.classList.add("hidden");
+  nav.classList.remove("animate-nav-forward");
+  nav.classList.add("animate-nav-back");
+  // nav.classList.add("hidden");
   brightLogo.classList.remove("hidden");
+});
+
+normalLogo.addEventListener("keypress", function(e){
+  if(e.key == 'Enter'){
+    nav.classList.add("animate-nav-back");
+    nav.classList.remove("animate-nav-forward");
+    // nav.classList.add("hidden");
+    brightLogo.classList.remove("hidden");
+  }
 });
