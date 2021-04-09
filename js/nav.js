c704602a76bec3871c0bcd465bb2e9aa04d904f6
EJ Nicolas <ejnicolas001@gmail.com>
2021-04-08 20:05:56 -0700

    Made nav better and tab-able

--- js/nav.js
+++ js/nav.js
@@ -5,11 +5,16 @@ console.log("Window width: "+width+"px");
 var brightLogo = document.querySelector("#bright-logo");
 var normalLogo = document.querySelector("#logo");
 var nav = document.querySelector("#side-nav");
+var navLinks = document.getElementsByClassName("nav-link");
 
 brightLogo.addEventListener("click", function(){
   nav.classList.add("animate-nav-forward");
   nav.classList.remove("animate-nav-back");
   nav.classList.remove("hidden");
+  normalLogo.classList.remove("hidden");
+  for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].classList.remove("hidden");
+  }
   brightLogo.classList.add("hidden");
 });
 
@@ -18,6 +23,10 @@ brightLogo.addEventListener("keypress", function(e){
     nav.classList.add("animate-nav-forward");
     nav.classList.remove("animate-nav-back");
     nav.classList.remove("hidden");
+    normalLogo.classList.remove("hidden");
+    for (var i = 0; i < navLinks.length; i++) {
+      navLinks[i].classList.remove("hidden");
+    }
     brightLogo.classList.add("hidden");
   }
 });
@@ -25,7 +34,10 @@ brightLogo.addEventListener("keypress", function(e){
 normalLogo.addEventListener("click", function(){
   nav.classList.remove("animate-nav-forward");
   nav.classList.add("animate-nav-back");
-  // nav.classList.add("hidden");
+  normalLogo.classList.add("hidden");
+  for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].classList.add("hidden");
+  }
   brightLogo.classList.remove("hidden");
 });
 
@@ -33,7 +45,10 @@ normalLogo.addEventListener("keypress", function(e){
   if(e.key == 'Enter'){
     nav.classList.add("animate-nav-back");
     nav.classList.remove("animate-nav-forward");
-    // nav.classList.add("hidden");
+    normalLogo.classList.add("hidden");
+    for (var i = 0; i < navLinks.length; i++) {
+      navLinks[i].classList.add("hidden");
+    }
     brightLogo.classList.remove("hidden");
   }
 });
